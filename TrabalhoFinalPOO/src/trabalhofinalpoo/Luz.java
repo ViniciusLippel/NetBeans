8883f90d5b34dc644c6a26472fec5b804508edd9
Vinicius Marzall Lippel <viniciusmlippel@gmail.com>
2019-11-27 15:21:47 -0300

    isso msm

--- TrabalhoFinalPOO/src/trabalhofinalpoo/Luz.java
+++ TrabalhoFinalPOO/src/trabalhofinalpoo/Luz.java
@@ -10,14 +10,47 @@ package trabalhofinalpoo;
  * @author vinicius.lippel
  */
 public class Luz {
-    private int anguloIncidencia;
-
-    public int getAnguloIncidencia() {
-        return anguloIncidencia;
+    private int inicialX;
+    private int inicialY;
+    private int angulo;
+    
+    public Luz(int inicialX, int inicialY, int angulo) {
+    	this.setInicialX(inicialX);
+    	this.setInicialY(inicialY);
+    	this.setAngulo(angulo);
     }
-
-    public void setAnguloIncidencia(int anguloIncidencia) {
-        this.anguloIncidencia = anguloIncidencia;
-    }
+    
+	public int getInicialX() {
+		return inicialX;
+	}
+	public void setInicialX(int inicialX) {
+		this.inicialX = inicialX;
+	}
+	public int getInicialY() {
+		return inicialY;
+	}
+	public void setInicialY(int inicialY) {
+		this.inicialY = inicialY;
+	}
+	public int getAngulo() {
+		return angulo;
+	}
+	public void setAngulo(int angulo) {
+		this.angulo = angulo;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Luz [inicialX=");
+		builder.append(inicialX);
+		builder.append(", inicialY=");
+		builder.append(inicialY);
+		builder.append(", angulo=");
+		builder.append(angulo);
+		builder.append("]");
+		return builder.toString();
+	}
+    
     
 }
--- TrabalhoFinalPOO/src/trabalhofinalpoo/Quadrado.java
+++ TrabalhoFinalPOO/src/trabalhofinalpoo/Quadrado.java
@@ -29,15 +29,21 @@ public class Quadrado {
         this.largura = largura;
     }
     
-    public int[][] gerarMatriz(){
-        int[][] matriz = new int[altura][largura];
+    public String[][] gerarMatriz(){
+        String[][] matriz = new String[altura][largura];
         for(int i=0; i<altura; i++){
-            matriz[i][0] = 1;
-            matriz[i][largura-1] = 1;
+            matriz[i][0] = "X";
+            matriz[i][largura-1] = "X";
         }
         for(int i=0; i<largura; i++){
-            matriz[0][i] = 1;
-            matriz[altura-1][i] = 1;
+            matriz[0][i] = "X";
+            matriz[altura-1][i] = "X";
+        }
+        
+        for(int i=1; i<altura-1; i++) {
+        	for(int x=1; x<largura-1; x++) {
+        		matriz[i][x] = " ";
+        	}
         }
         return matriz;
     }
