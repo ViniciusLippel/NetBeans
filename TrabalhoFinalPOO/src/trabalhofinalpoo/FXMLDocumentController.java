8812c72b9a13d52f6ceddf3299b3e84d6f26dacb
Vinicius Marzall Lippel <viniciusmlippel@gmail.com>
2019-12-04 22:37:24 -0300

    projetinho

--- TrabalhoFinalPOO/src/trabalhofinalpoo/FXMLDocumentController.java
+++ TrabalhoFinalPOO/src/trabalhofinalpoo/FXMLDocumentController.java
@@ -5,6 +5,11 @@
  */
 package trabalhofinalpoo;
 
+import java.awt.Button;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
@@ -23,7 +28,7 @@ import javafx.scene.control.TextField;
  */
 public class FXMLDocumentController implements Initializable {
     
-        @FXML
+    @FXML
     private ChoiceBox<?> tipoObj;
 
     @FXML
@@ -48,11 +53,36 @@ public class FXMLDocumentController implements Initializable {
     private Button salvar;
 
     @FXML
-    private Label teste;
+    private TextField nomeArquivo;
+
+    @FXML
+    private Button verDadosUp;
+
+    @FXML
+    private Label anguloUp;
+
+    @FXML
+    private Label posXUp;
+
+    @FXML
+    private Label posYUp;
+
+    @FXML
+    private Label tipoUp;
+
+    @FXML
+    private Label larguraUp;
+
+    @FXML
+    private Label alturaUp;
+
+    @FXML
+    private Button geraGrafUp;
+
 
     @FXML
     public void gerarGrafico(ActionEvent event) throws InterruptedException {
-    	Luz l = new Luz();
+        Luz l = new Luz();
 		l.setAngulo(Angulos.A45);
 		l.setX(Integer.parseInt(posx.getText()));
 		l.setY(Integer.parseInt(posy.getText()));
@@ -91,11 +121,63 @@ public class FXMLDocumentController implements Initializable {
     }
     
     @FXML
-    public void salvar(ActionEvent event){
-        
+    public void salvar(ActionEvent event) throws IOException{
+        Luz l = new Luz();
+        l.setAngulo(Angulos.A45);
+        l.setX(Integer.parseInt(posx.getText()));
+        l.setY(Integer.parseInt(posy.getText()));
+
+        Matriz m = new Matriz();
+        m.setLargura(Integer.parseInt(largura.getText()));
+        m.setAltura(Integer.parseInt(altura.getText()));
+        Json json = new Json();
+	json.gravar(l, m, "teste");
     }
     
-    @Override
+    @FXML
+    public void verDados(ActionEvent event){
+    	Json json = new Json();
+    	Grafico g = new Grafico();
+		Persistencia per = new Persistencia (json);
+		g = per.ler(nomeArquivo.getText());
+		anguloUp.setText(Integer.toString(g.getLuz().anguloToInt()));
+		posXUp.setText(Integer.toString(g.getLuz().getX()));
+		posYUp.setText(Integer.toString(g.getLuz().getY()));
+//		tipoUp.setText(g.getMatriz().);
+		alturaUp.setText(Integer.toString(g.getMatriz().getAltura()));
+		alturaUp.setText(Integer.toString(g.getMatriz().getLargura()));
+    }
+    
+    @FXML
+    public void gerarGraficoUpload(ActionEvent event) throws InterruptedException {
+    	Luz l = new Luz();
+		l.setAngulo(Angulos.A45);
+		l.setX(Integer.parseInt(posXUp.getText()));
+		l.setY(Integer.parseInt(posYUp.getText()));
+		
+		Matriz m = new Matriz();
+		m.setLargura(Integer.parseInt(larguraUp.getText()));
+		m.setAltura(Integer.parseInt(alturaUp.getText()));
+		
+		Grafico g = new Grafico();
+		g.setLuz(l);
+		g.setMatriz(m);
+		g.montarGrafico();
+		
+		mostraMatriz(g.getGrafico());
+		
+		boolean repeat = true;
+		int inicialX = l.getX();
+		int inicialY = l.getY();
+		while(repeat){
+			g.andarLuz();
+			mostraMatriz(g.getGrafico());
+			if(g.getLuz().getX() == inicialX && g.getLuz().getY() == inicialY) 
+				repeat = false;
+			Thread.sleep(100);
+		}
+    }
+    
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
